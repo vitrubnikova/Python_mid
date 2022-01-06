406ce5772d2233e4fee88a3a0cf72494dbf4a58e
vitrubnikova <vitrubnikova@gmail.com>
2022-01-07 00:35:14 +0300

    Add files via upload

---
+++ 7/app7.py
@@ -0,0 +1,22 @@
+import os, datetime
+
+def collector(path, res_path):
+    res_path = os.path.normpath(res_path)
+    path = os.path.normpath(path)
+    for dirpath, dirnames, filenames in os.walk(path):
+        for file in filenames:
+            file_time = os.path.getmtime(f"{dirpath}\\{file}")
+            datetime_file = datetime.datetime.fromtimestamp(file_time)
+            file_date = datetime_file.strftime("%d.%m.%Y")
+
+            if os.path.isdir(f"{res_path}\\{file_date}"):
+                os.replace(f"{dirpath}\\{file}", f"{res_path}\\{file_date}\\{file}")
+            else:
+                os.mkdir(f"{res_path}\\{file_date}\\")
+                os.replace(f"{dirpath}\\{file}", f"{res_path}\\{file_date}\\{file}")
+
+
+path = "C:\\Users\\User\\test"
+res_path = "C:\\Users\\User\\test"
+
+collector(path, res_path)
